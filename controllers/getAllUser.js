2261a2a59eae348ff020dd0c59fff249709977d5
harsh <93788928+harsh23079@users.noreply.github.com>
2024-07-05 17:15:30 +0530

    getAllUser api created

--- controllers/getAllUser.js
+++ controllers/getAllUser.js
@@ -1,5 +1,12 @@
-const getAllUser = async () => {
+const User = require("../models/user.js");
+
+const getAllUser = async (req, res) => {
   try {
-    
-  } catch (err) {}
+    const reqData = await User.find();
+    return res.status(200).json(reqData);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 };
+
+module.exports = getAllUser;
--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,11 @@
-const express=require("express");
+const express = require("express");
 const { register, login } = require("../controllers/auth");
-const router=express.Router();
-router.post("/register",register);
+const getAllUser = require("../controllers/getAllUser.js");
+const router = express.Router();
+router.post("/register", register);
 router.post("/login", login);
 
-module.exports=router;
+router.get("/users", getAllUser);
+
+
+module.exports = router;
